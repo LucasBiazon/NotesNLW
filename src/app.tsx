057c8110f578979622bbde21d3d8158a096d71d0
Lucas <lucasbiazonpalma@gmail.com>
2024-02-13 12:45:07 -0300

    Modal card

--- src/app.tsx
+++ src/app.tsx
@@ -5,8 +5,8 @@ import { NoteCard } from './components/note-card';
 export function App() {
 
   return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
-      <img src={logo} alt="logo nlw" className='w-64'/>
+    <div className='mx-auto px-6 xl:px-0 max-w-6xl my-12 space-y-6 '>
+      <img src={logo} alt="logo nlw" className='w-48 sm:w-64'/>
       <form>
         <input 
         type="text" 
@@ -18,7 +18,7 @@ export function App() {
       <div className='h-px bg-slate-500'>
       </div>
 
-      <div className='grid grid-cols-3 auto-rows-[248px] gap-6'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 auto-rows-[248px] gap-6'>
 
         <div className='roundend-md bg-slate-700 p-5 space-y-3'>
           <span className='text-small text-slate-200'>
@@ -29,9 +29,12 @@ export function App() {
           </p>
         </div>
 
-        <NoteCard></NoteCard>
-        <NoteCard></NoteCard>
-        <NoteCard></NoteCard>
+        <NoteCard
+          note={{
+            date: new Date(), 
+            content:'Good'
+          }}
+        />
       </div>
     </div>
   )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,13 +1,50 @@
-export function NoteCard(){
+import * as Dialog from '@radix-ui/react-dialog'
+import {formatDistanceToNow} from 'date-fns';
+import {ptBR} from 'date-fns/locale';
+import {X} from 'lucide-react'
+interface NoteCardPros {
+  note: 
+  {
+    date: Date;
+    content: string
+  } 
+}
+
+export function NoteCard({note}: NoteCardPros){
   return (
-    <button className='text-left outline-none roundend-md bg-slate-800 p-5 space-y-3 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-sky-400 focus:ring-2 focus:ring-sky-400'>
-        <span className='text-small text-slate-300'>
-           há 2dias
-        </span>
-         <p className='text-small text-slate-400 leading-6'>
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero quo saepe, molestiae in temporibus error hic consequatur beatae sit possimus provident quae doloremque, facere cum cumque iure culpa. Quia, alias?
-        </p>
-        <div className='absolute bottom-0 right-0 left-0 h-1/2 pointer-events-none bg-gradient-to-t from-black/60 to-black/0'></div>
-    </button>
+    <Dialog.Root>
+      <Dialog.Trigger className='text-left outline-none roundend-md bg-slate-800 p-5 flex flex-col gap-5 overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-sky-400 focus:ring-2 focus:ring-sky-400'>
+          <span className='text-small text-slate-300'>
+            {formatDistanceToNow(note.date, {locale:ptBR, addSuffix: true})}
+          </span>
+          <p className='text-small text-slate-400 leading-6 break-words'>
+            {note.content}
+          </p>
+          <div className='absolute bottom-0 right-0 left-0 h-1/2 pointer-events-none bg-gradient-to-t from-black/60 to-black/0'></div>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className='inset-0 fixed bg-black/60'/>
+        <Dialog.Content className='z-10  overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[360px] sm:max-w-2xl w-full h-[60vh] outline-none rounded-lg bg-slate-700 flex flex-col'>
+          <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-200'>
+            <X className='inset-5'></X>
+          </Dialog.Close>
+         <div className='flex flex-1 flex-col p-5'>
+            <span className='text-small text-slate-300'>
+              {note.date.toISOString()}
+            </span>
+            <p className='text-small text-slate-400 leading-6 break-words'>
+              {note.content}
+            </p>
+         </div>
+         <button 
+         className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
+         type='button'
+         >
+          Deseja <span className='text-red-400 group-hover:underline'>apagar esta nota?</span>
+         </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
